9ae22d90f2d32a67b1e768c5f5615a60e1fcd0dc
pcochoco <cochocolatecoo@gmail.com>
2023-01-09 23:23:28 +0900

    cmg

--- B3052.java
+++ B3052.java
@@ -2,21 +2,28 @@ import java.util.Scanner;
 public class B3052{
 
 	public static void main(String[] args){
-		boolean[] arr=new boolean[42]; 
-		//나머지가 나올 수 있는 범위 0~41=> 42개
-		//boolean 42개 인덱스 배열 생성  
+		int[] arr=new int[10]; 
+	
 		int num;
 		int count=0;
+		boolean b1=false;
 		Scanner sc=new Scanner(System.in);
 
 		for(int i=0;i<10;i++){ //10개 수 입력받음 
 			num=sc.nextInt();
-			arr[num%42]=true;
-			//서로 다른 값 몇개 있는지 출력 
+			arr[i]=num%42;
+			for(int j=i+1;j<arr.length;j++){
+				if(arr[i]==arr[j]){
+					b1=true;
+					break;
+				}
+			
+			}
+			if(b1==false){
+				count++;
+			}
 		}
-		for(boolean v: arr){
-			if(v) count++; //arr[인덱스]가 true이면 개수 셈  
-		}
+		
 		System.out.println(count);
 
 		sc.close();
