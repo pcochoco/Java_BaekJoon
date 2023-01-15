dc5e8bf8a991a3b878f98d848c97fb270e4251cc
pcochoco <cochocolatecoo@gmail.com>
2023-01-16 01:25:50 +0900

    commiting

---
+++ B10773.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class B10773 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int K;
+        int sum=0;
+        int n;
+        K=sc.nextInt(); //주어지는 정수의 개수 
+        Stack<Integer> stack=new Stack<Integer>(); //integer 형태 stack 선언 
+   
+
+        for(int i=0;i<K;i++){ 
+            n=sc.nextInt(); //K번 동안 정수 입력받음 
+            if(n==0){
+                stack.pop(); //0인 경우 가장 최근 수 지움 
+            
+            }
+            else{ //아닐 경우 해당 수 씀 
+                
+                stack.push(n);
+                //sum을 여기서 하면 다음의 입력 (0)과 상관없는 모든 수의 합 계산 
+            }
+        }
+
+        for(int f:stack){
+            sum+=f; //추가한 모든 합 구함
+        }
+        System.out.println(sum);
+        sc.close();
+    }    
+}
---
+++ B10828.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class B10828 {
+    
+    public static int[] stack;
+    public static int size=0;
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        StringBuffer sb=new StringBuffer();
+        //System.out.println의 줄바꿈보다 시간이 덜 걸림 
+        int N=sc.nextInt(); //명령어의 개수 
+        stack=new int[N];
+
+        for(int i=0;i<N;i++){
+            String inst=sc.next(); //명령어 입력 
+
+            switch(inst){
+                case "push": //push 인 경우 
+                    push(sc.nextInt()); //따라오는 숫자 입력받음 
+                    break;
+
+                case "pop":
+                    sb.append(pop()).append('\n'); //enter 추가 
+                    break;
+            
+        
+                case "size":
+                    sb.append(size()).append('\n');
+                    break;
+
+                case "empty":
+                    sb.append(empty()).append('\n');
+                    break;
+
+                case "top": //else if(inst=="top")
+                    sb.append(top()).append('\n');
+                    break;
+            }
+
+        }
+    
+        System.out.println(sb);
+        sc.close();
+
+    }
+    public static void push(int item){
+        stack[size]=item; //size-1이 마지막이므로
+        size++; //size 증가 
+    }
+    public static int pop(){
+        if(size==0){ //비었을 경우 불가 
+            return -1;
+        }
+        else{
+            int res=stack[size-1]; //마지막 값(뺄 값) 저장 
+            size--;
+            return res; 
+        }
+    }
+    public static int size(){
+        return size; //변수로 길이 변동하므로 
+    }
+    public static int empty(){
+        if(size==0){
+            return 1;
+        }
+        else{
+            return 0; 
+        }
+    }
+    public static int top(){
+        if(size==0){ //비었으면 불가 
+            return -1; 
+        }
+        else{
+            return stack[size-1];
+        }
+    }
+
+
+
+}
